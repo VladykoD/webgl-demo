11faf9d1c312a3a78fa7cfbfc64c5fab681322e3
VladykoD <VladykoD@gmail.com>
2021-03-23 04:55:29 +0300

    added base for webgl: detection, fill, simple animation, clicks handler

---
+++ WebGL-base/app.js
@@ -0,0 +1,58 @@
+(() => {
+    //detect webgl
+    let canvas = document.createElement('canvas');
+    document.body.appendChild(canvas);
+    let gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+
+    if (!gl) {
+        console.log('Failed to get WebGL context. Your browser or device may not support WebGL.')
+        return;
+    }
+    console.log('Your browser supports WebGL.')
+
+
+    //fill in green color
+    gl.viewport(0,0,gl.drawingBufferWidth, gl.drawingBufferHeight);
+    gl.clearColor(0.0, 0.5, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT)
+
+
+    //change background after click
+    let button = document.querySelector('#switch-bg')
+    button.addEventListener('click', switchColor, false)
+
+    function switchColor() {
+        let color = getRandomColor();
+        gl.clearColor(color[0], color[1], color[2], 1.0);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+    }
+
+    function getRandomColor() {
+        return [Math.random(), Math.random(), Math.random()]
+    }
+
+
+    //change background animation
+    let buttonAnim = document.querySelector('#animate-bg');
+    let state = buttonAnim.querySelector('b');
+    let timer;
+
+    function startAnimation(e) {
+        buttonAnim.removeEventListener(e.type, startAnimation, false);
+        buttonAnim.addEventListener('click', stopAnimation, false);
+        state.textContent = 'off';
+
+        timer = setInterval(switchColor, 1000);
+        switchColor();
+    }
+
+    function stopAnimation(e) {
+        buttonAnim.removeEventListener(e.type, stopAnimation, false);
+        buttonAnim.addEventListener('click', startAnimation, false);
+        state.textContent = 'on';
+        clearInterval(timer);
+    }
+    stopAnimation({type: 'click'});
+
+
+})();
